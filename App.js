aeb69580aed1efe71f3f20066fcd892ccbec9a6c
github username <github email address>
2020-05-25 22:56:14 -0600

    First commit

--- App.js
+++ App.js
@@ -1,19 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native'
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+
+  // ES5
+  // function goalInputHandler(enteredText) {
+  //   setEnteredGoal(enteredText)
+  // }
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals([
+      ...courseGoals,
+      { key: Math.random().toString(), value: enteredGoal }
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal} />
+        <Button title="ADD" onPress={addGoalHandler} />
+      </View>
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
-});
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  textInput: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%'
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+  }
+})
